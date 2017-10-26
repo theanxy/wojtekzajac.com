447a5f82861b8768831e2571d3561b0530f2d3f6
Wojtek Zając <wojtek@wojtekzajac.com>
2017-10-26 16:08:47 +0800

    Fixes to paths

--- src/layouts/_menu.js
+++ src/layouts/_menu.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
-const Menu = styled.ul`float: right;`;
+const Menu = styled.ul`
+  float: right;
+`;
 
 const Li = styled.li`
   display: inline-block;
@@ -14,37 +16,36 @@ const pages = [
   {
     id: 0,
     text: 'Home',
-    url: ''
+    url: '/'
   },
   {
     id: 4,
     text: 'Sample',
-    url: 'sample'
+    url: '/sample'
   },
   {
     id: 1,
     text: 'Experience',
-    url: 'experience'
+    url: '/experience'
   },
   {
     id: 2,
     text: 'Speaking',
-    url: 'speaking'
+    url: '/speaking'
   },
   {
     id: 3,
     text: 'Travels',
-    url: 'travels'
+    url: '/travels'
   }
 ];
 
-export default () =>
+export default () => (
   <Menu>
-    {pages.map(page =>
+    {pages.map(page => (
       <Li key={page.id}>
-        <Link to={page.url}>
-          {page.text}
-        </Link>
+        <Link to={page.url}>{page.text}</Link>
       </Li>
-    )}
-  </Menu>;
+    ))}
+  </Menu>
+);
