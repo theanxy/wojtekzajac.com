6e001c20797bff48e2463ac56f7d91788769eff9
Wojtek Zając <wojtek@wojtekzajac.com>
2018-06-13 16:32:57 +0200

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  display: flex;
+  // display: flex;
 `;
 
 const About = styled.article`
@@ -22,39 +22,42 @@ class Experience extends React.Component {
     return (
       <div>
         <Container>
-          <img
-            src="//placehold.it/300x500"
-            width="300"
-            height="500"
-            alt="Wojtek"
-          />
+          <img src="//placehold.it/1400x200" alt="Wojtek" />
 
           <About>
             <p>
-              I've got 17 years of experience with front-end development and 7
-              years of experience with leading distributed teams.
+              I've got 17 years of experience with front-end development and 7 years of experience with leading
+              distributed teams.
             </p>
             <p>Howdy. I am a front-end engineer based in Kraków, Poland.</p>
 
             <p>
-              I specialize in JavaScript, web accessibility and user experience
-              engineering. I’ve started the Kraków office of Xfive.co. I
-              currently work with X-Team.com.
+              I specialize in JavaScript, web accessibility and user experience engineering. I’ve started the Kraków
+              office of Xfive.co. I currently work with X-Team.com.
             </p>
 
             <p>
-              Typography (especially vintage book covers) is my obsession. I
-              sometimes pretend to be a public speaker. Other times I take
-              photos with Instax or my phone.
+              Typography (especially vintage book covers) is my obsession. I sometimes pretend to be a public speaker.
+              Other times I take photos with Instax or my phone.
             </p>
           </About>
         </Container>
 
-        <h2>Experience</h2>
-        <h3>Development</h3>
-        <p>asdf</p>
-        <h3>Leadership</h3>
-        <p>sadf</p>
+        <h2>Interviews</h2>
+
+        <section>
+          <h2>Experience</h2>
+          <div>
+            <h3>Development</h3>
+            <p>asdf</p>
+          </div>
+          <div>
+            <h3>Leadership</h3>
+            <p>sadf</p>
+          </div>
+        </section>
+
+        <h2>Testimonials?</h2>
       </div>
     );
   }
