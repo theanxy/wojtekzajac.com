72ea7d891cc44ae459202685e76c2e4fb7f379b6
Wojtek Zając <wojtek@wojtekzajac.com>
2018-06-22 00:05:01 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-242891-5',
+        respectDNT: true,
+        exclude: []
+      }
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`
